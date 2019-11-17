971b1013aee75f2a6b6fc618d33bd45f184ac0e1
TC1995 <mariogplayer90@gmail.com>
2019-11-17 01:06:02 +0100

    Initial AMD PCnet commit.

--- src/network/network.c
+++ src/network/network.c
@@ -62,6 +62,7 @@
 #include "network.h"
 #include "net_3c503.h"
 #include "net_ne2000.h"
+#include "net_pcnet.h"
 #include "net_wd8003.h"
 
 
@@ -69,6 +70,8 @@ static netcard_t net_cards[] = {
     { "None",				"none",		NULL,
       NULL								},
     { "[ISA] 3Com EtherLink II (3C503)","3c503",	&threec503_device,
+      NULL								},
+    { "[ISA] AMD PCnet-ISA",	"pcnetisa",	&pcnet_isa_device,
       NULL								},
     { "[ISA] Novell NE1000",		"ne1k",		&ne1000_device,
       NULL								},
@@ -88,7 +91,11 @@ static netcard_t net_cards[] = {
       NULL								},
     { "[MCA] Western Digital WD8003E/A", "wd8003ea",	&wd8003ea_device,
       NULL								},
+    { "[PCI] AMD PCnet-PCI",	"pcnetpci",	&pcnet_pci_device,
+      NULL								},
     { "[PCI] Realtek RTL8029AS",	"ne2kpci",	&rtl8029as_device,
+      NULL								},
+    { "[VLB] AMD PCnet-VL",	"pcnetvlb",	&pcnet_vlb_device,
       NULL								},
     { "",				"",		NULL,
       NULL								}
@@ -99,6 +106,7 @@ static netcard_t net_cards[] = {
 int		network_type;
 int		network_ndev;
 int		network_card;
+static volatile int	net_wait = 0;
 char		network_host[522];
 netdev_t	network_devs[32];
 #ifdef ENABLE_NIC_LOG
@@ -137,6 +145,32 @@ network_log(const char *fmt, ...)
 #define network_log(fmt, ...)
 #endif
 
+/*
+ * Return the 6-bit index into the multicast
+ * table. Stolen unashamedly from FreeBSD's if_ed.c
+ */
+int
+mcast_index(const void *dst)
+{
+#define POLYNOMIAL 0x04c11db6
+    uint32_t crc = 0xffffffffL;
+    int carry, i, j;
+    uint8_t b;
+    uint8_t *ep = (uint8_t *)dst;
+
+    for (i=6; --i>=0;) {
+	b = *ep++;
+	for (j = 8; --j >= 0;) {
+		carry = ((crc & 0x80000000L) ? 1 : 0) ^ (b & 0x01);
+		crc <<= 1;
+		b >>= 1;
+		if (carry)
+			crc = ((crc ^ POLYNOMIAL) | carry);
+	}
+    }
+    return(crc >> 26);
+#undef POLYNOMIAL
+}
 
 void
 network_wait(uint8_t wait)
@@ -219,6 +253,8 @@ network_attach(void *dev, uint8_t *mac, NETRXCB rx)
     net_cards[network_card].priv = dev;
     net_cards[network_card].rx = rx;
     network_mac = mac;
+
+	network_set_wait(0);
 
     /* Create the network events. */
     poll_data.wake_poll_thread = thread_create_event();
@@ -441,4 +477,21 @@ network_card_get_from_internal_name(char *s)
     }
 	
     return(-1);
+}
+
+
+void
+network_set_wait(int wait)
+{
+    network_busy(1);
+    net_wait = wait;
+    network_busy(0);
+}
+
+
+int
+network_get_wait(void)
+{
+	pclog("Network: net wait = %02x\n", net_wait);
+    return net_wait;
 }
